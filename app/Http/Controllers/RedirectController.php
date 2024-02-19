e65699781863e4e27f8cb76ed0b907872e96e927
wijayad6 <denywijaya296@gmail.com>
2024-02-19 11:43:14 +0700

    commit

--- app/Http/Controllers/RedirectController.php
+++ app/Http/Controllers/RedirectController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class RedirectController extends Controller
 {
-    //
+    public function cek()
+    {
+        if (auth()->user()->role_id === 1) {
+            return redirect('dashboard_admin');
+        }elseif(auth()->user()->role_id === 2) {
+            return redirect('dashboard_petugas');
+        }else{
+            return redirect('dashboard_user');
+        }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PetugasController;
+use App\Http\Controllers\RedirectController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,8 +18,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function ()
+{
     return view('auth.login');
 });
 
+Route::group(['middleware' => 'guest'], function()
+{
+    Route::get('/', [AuthController::class, 'login'])->name('login');
+    Route::post('/', [AuthController::class, 'loginAction']);
+});
 
+Route::group(['middleware' => ['auth','checkrole:1,2,3']], function()
+{
+    Route::get('/logout', [AuthController::class, 'logout']);
+    Route::post('/redirect', [RedirectController::class, 'cek']);
+});
+
+Route::group(['middleware' => ['auth', 'checkrole:1']], function()
+{
+    Route::get('admin', [AdminController::class, 'index']);
+});
+Route::group(['middleware' => ['auth', 'checkrole:2']], function()
+{
+    Route::get('petugas', [PetugasController::class, 'index']);
+});
+Route::group(['middleware' => ['auth', 'checkrole:3']], function()
+{
+    Route::get('user', [UserController::class, 'index']);
+});
+
+Route::controller(AuthController::class)->group(function()
+{
+    Route::get('register', 'register')->name('register');
+    Route::get('register', 'registerSave')->name('register.save');
+
+    Route::get('login', 'login')->name('login');
+    Route::get('login', 'loginAction')->name('login.action');
+
+    Route::get('logout', 'logout')->middleware('auth')->name('logout');
+});
