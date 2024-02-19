629e13fffd43ddbe001b343f036e0274d8e1ee55
wijayad6 <denywijaya296@gmail.com>
2024-02-19 12:08:30 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('auth.login');
+    return view('/');
 });
 
 Route::group(['middleware' => 'guest'], function () {
